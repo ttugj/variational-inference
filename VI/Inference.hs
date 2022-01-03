f638add4ada46c5d16a2faaa68cc1c419ce294ac
Jan Gutt <jangutt@trdlnk.com>
2022-01-03 17:08:18 +0100

    rewrite VI.Inference

--- VI/Inference.hs
+++ VI/Inference.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE UnicodeSyntax, PolyKinds, DataKinds, TypeFamilies, TypeOperators, GADTs, ConstraintKinds, TypeApplications, AllowAmbiguousTypes, NoImplicitPrelude, UndecidableInstances, NoStarIsType, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, LiberalTypeSynonyms, ScopedTypeVariables, InstanceSigs, DefaultSignatures, RankNTypes, TupleSections, BangPatterns #-}
 
 module VI.Inference ( -- * SGD on divergence
-                      sgdStep, sgdSteps, sgdMonitor
+                      Loss, Optimiser, OptimiserState(..)
+                    , chunked, monitor
+                    , plainSGD
+                    , demo
                     ) where
 
 import VI.Categories
@@ -24,49 +27,55 @@ import System.IO
 import qualified Numeric.LinearAlgebra.Static as LA
 import qualified Data.List as L
 
--- | Elementary SGD step, acting on the associated Euclidean space and reporting sampled loss. 
-sgdStep  ∷ (Monad m, Domain x, Dim x ~ n) 
-         ⇒ Double           -- ^ rate
-         → m (Mor x (ℝ 1))  -- ^ loss sampler
-         → LA.R n → m (LA.R n, Double)
-sgdStep rate loss p = let go (Mor (J φ)) = let (γ, dγ) = φ p
-                                               (g, _)  = LA.headTail γ
-                                            in (p - LA.konst rate * dγ 1, g)
-                       in go <$> loss
-           
--- | As in 'sgdStep', but preforms a given number of steps and reports average loss.
-sgdSteps ∷ (Monad m, Domain x, Dim x ~ n) 
-         ⇒ Int              -- ^ number of steps
-         → Double           -- ^ rate 
-         → m (Mor x (ℝ 1))  -- ^ loss sampler
-         → LA.R n → m (LA.R n, Double)
-sgdSteps steps rate loss p = bimap id (/ fromIntegral steps) <$> go steps p 0 where
-                                    go 0 !q !a = return (q, a)
-                                    go l !q !a = sgdStep rate loss q >>= \(q', a') → go (l-1) q' (a+a')
+type Loss      m   x = m (Mor x (ℝ 1))
+type Optimiser m s x = Loss m x → s → LA.R (Dim x) → m (s, LA.R (Dim x), Double) 
 
--- | Debugging
-sgdMonitor ∷ ∀ x. Domain x
-           ⇒ Int    -- ^ number of steps per epoch
-           → Double -- ^ rate
-           → (∀ m. SampleM m ⇒ m (Mor x (ℝ 1))) -- ^ loss sampler
-           → Maybe (Mor Pt x)                   -- ^ initial point
-           → IO ()
-sgdMonitor steps rate loss init
-           = executeSampleIO $ go p0 0 where
-               p0 = case init of
-                      Just (Mor (J φ)) → pr1 (φ 0)
-                      Nothing → 0
-               go ∷ ∀ m. (SampleM m, MonadIO m) ⇒ LA.R (Dim x) → Int → m ()
-               go p i = do
-                           (p', g) ← sgdSteps steps rate loss p
-                           liftIO . putStrLn . L.unwords $ [ "epoch", show i, "loss", show g, "point", show p' ]
-                           go p' (i+1)
+class Domain x ⇒ OptimiserState s x where
+    initState ∷ s    
 
-sgdDemo ∷ IO ()
-sgdDemo = putStrLn "* target: 3.14, 1.00" >> sgdMonitor steps rate loss Nothing
+instance Domain x ⇒ OptimiserState () x where
+    initState = ()
+
+-- | Perform optimisation steps in chunks, reporting averaged loss.
+chunked ∷ Monad m ⇒ Int → Optimiser m s x → Optimiser m s x 
+chunked steps opt loss s0 p0 
+        = go s0 p0 0 steps where
+            go !s !p !acc 0 = return (s, p, acc / fromIntegral steps)
+            go !s !p !acc k = do
+                                (s',p',g) ← opt loss s p
+                                go s' p' (acc+g) (k-1)
+
+-- | Run chunked steps of an optimiser, displaying loss and location.
+monitor ∷ ∀ s x. OptimiserState s x
+        ⇒ (∀ m. Functor m ⇒ Optimiser m s x)
+        → (∀ m. SampleM m ⇒ Loss m x) 
+        → Maybe (Mor Pt x)
+        → Int
+        → IO ()
+monitor opt loss init chunk 
+        = executeSampleIO $ go s0 p0 0 where
+            s0 = initState @s @x
+            p0 = case init of
+                   Just (Mor (J φ)) → pr1 (φ 0)
+                   Nothing → 0
+            go ∷ ∀ m. (SampleM m, MonadIO m) ⇒ s → LA.R (Dim x) → Int → m ()
+            go s p i = do
+                   (s', p', g) ← chunked chunk opt loss s p
+                   liftIO . putStrLn . L.unwords $ [ "epoch", show i, "loss", show g, "point", show p' ]
+                   go s' p' (i+1)
+
+-- | A plain constant-rate SGD optimiser
+plainSGD ∷ Functor m 
+         ⇒ Double  -- ^ rate
+         → Optimiser m () x
+plainSGD rate loss _ p = let go (Mor (J φ)) = let (γ, dγ) = φ p
+                                                  (g, _)  = LA.headTail γ
+                                               in ((), p - LA.konst rate * dγ 1, g)
+                          in go <$> loss
+
+demo ∷ IO ()
+demo = putStrLn "* target: 3.14, 1.00" >> monitor (plainSGD 0.001) loss Nothing 1000
             where
-                    steps = 1000
-                    rate  = 0.001
                     loss  ∷ ∀ m. SampleM m ⇒ m (Mor ((ℝ 1, ℝp 1), Pt) (ℝ 1))
                     loss  = divergenceSample variationalFamily posterior
                     variationalFamily ∷ Couple Density Sampler (ℝ 1, ℝp 1) (ℝ 1)
